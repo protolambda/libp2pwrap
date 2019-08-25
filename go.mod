8c4807e04c43a7fe1b046a1c95da454429525e44
protolambda <proto@protolambda.com>
2019-08-25 14:36:29 +0200

    libp2p secio daemon

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/protolambda/libp2pwrap
 go 1.12
 
 require (
+	github.com/libp2p/go-libp2p v0.3.1
 	github.com/libp2p/go-libp2p-core v0.2.2
 	github.com/libp2p/go-libp2p-daemon v0.2.1
+	github.com/libp2p/go-libp2p-secio v0.2.0
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/multiformats/go-multiaddr v0.0.4
--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/peer"
+	p2pd "github.com/libp2p/go-libp2p-daemon"
 	"github.com/libp2p/go-libp2p-daemon/p2pclient"
+	secio "github.com/libp2p/go-libp2p-secio"
 	"github.com/multiformats/go-multiaddr"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 var controlAddrStr = flag.String("control", "", "control multiaddress")
 var listenAddrStr = flag.String("listen", "", "listen multiaddress")
 var peerIDStr = flag.String("peerID", "", "peer ID")
+
+func check(err error, msg string) {
+	if err != nil {
+		panic(fmt.Errorf("%s: %v", msg, err))
+	}
+}
 
 func main() {
 	flag.Usage = func() {
@@ -20,16 +32,37 @@ func main() {
 	}
 	flag.Parse()
 
-	check := func (err error, msg string) {
-		if err != nil {
-			panic(fmt.Errorf("%s: %v", msg, err))
-		}
-	}
+
 	controlAddr, err := multiaddr.NewMultiaddr(*controlAddrStr)
 	check(err, "invalid control addr")
 	listenAddr, err := multiaddr.NewMultiaddr(*listenAddrStr)
 	check(err, "invalid listen addr")
 
+	security := libp2p.Security(secio.ID, secio.New)
+
+	options := []libp2p.Option{
+		security,
+	}
+
+	ctx := context.Background()
+	d, err := p2pd.NewDaemon(ctx, controlAddr, "", options...)
+	check(err, "failed to create new daemon")
+
+	// concurrently connect to our new daemon
+	go connect(controlAddr, listenAddr)
+
+	// now wait for stop signal to close daemon
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT)
+
+	select {
+	case <-stop:
+		d.Close()
+		os.Exit(0)
+	}
+}
+
+func connect(controlAddr, listenAddr multiaddr.Multiaddr) {
 	var peerAddrs []multiaddr.Multiaddr
 	for i, addrStr := range flag.Args() {
 		addr, err := multiaddr.NewMultiaddr(addrStr)
